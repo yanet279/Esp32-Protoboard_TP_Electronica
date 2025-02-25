c53eeaee308ccc1834a97089a7d91e54e84f42c1
Yanet Choque <sol3127g@gmaill.com>
2025-02-24 23:44:17 -0300

    Se agrego el FakePin para simular machine

--- main.py
+++ main.py
@@ -1,25 +1,54 @@
 import random
-
 import time
-import machine
 
-bd0 = machine.Pin(23, machine.Pin.OUT)
-bd1 = machine.Pin(22, machine.Pin.OUT)
-bd2 = machine.Pin(21, machine.Pin.OUT)
-bd3 = machine.Pin(19, machine.Pin.OUT)
-bd4 = machine.Pin(18, machine.Pin.OUT)
-bd5 = machine.Pin(5, machine.Pin.OUT)
-bd6 = machine.Pin(4, machine.Pin.OUT)
-bd7 = machine.Pin(2, machine.Pin.OUT)
-bi0 = machine.Pin(32, machine.Pin.OUT)
-bi1 = machine.Pin(33, machine.Pin.OUT)
-bi2 = machine.Pin(25, machine.Pin.OUT)
-bi3 = machine.Pin(26, machine.Pin.OUT)
-bi4 = machine.Pin(27, machine.Pin.OUT)
-bi5 = machine.Pin(14, machine.Pin.OUT)
-bi6 = machine.Pin(12, machine.Pin.OUT)
-bi7 = machine.Pin(13, machine.Pin.OUT)
+# import machine
 
+# Para ESP32
+# bd0 = machine.Pin(23, machine.Pin.OUT)
+# bd1 = machine.Pin(22, machine.Pin.OUT)
+#bd2 = machine.Pin(21, machine.Pin.OUT)
+#bd3 = machine.Pin(19, machine.Pin.OUT)
+#bd4 = machine.Pin(18, machine.Pin.OUT)
+#bd5 = machine.Pin(5, machine.Pin.OUT)
+#bd6 = machine.Pin(4, machine.Pin.OUT)
+#bd7 = machine.Pin(2, machine.Pin.OUT)
+#bi0 = machine.Pin(32, machine.Pin.OUT)
+#bi1 = machine.Pin(33, machine.Pin.OUT)
+#bi2 = machine.Pin(25, machine.Pin.OUT)
+#bi3 = machine.Pin(26, machine.Pin.OUT)
+#bi4 = machine.Pin(27, machine.Pin.OUT)
+#bi5 = machine.Pin(14, machine.Pin.OUT)
+#bi6 = machine.Pin(12, machine.Pin.OUT)
+#bi7 = machine.Pin(13, machine.Pin.OUT)
+
+#Simulación de machine.Pin
+class FakePin:
+    def __init__(self, pin_number, mode):
+        self.pin_number = pin_number
+        self.state = False
+    
+    def value(self, val=None):
+        if val is not None:
+            self.state = val
+        return self.state
+
+#SIN ESP32
+bd0 = FakePin(23, None)
+bd1 = FakePin(22, None)
+bd2 = FakePin(21, None)
+bd3 = FakePin(19, None)
+bd4 = FakePin(18, None)
+bd5 = FakePin(5, None)
+bd6 = FakePin(4, None)
+bd7 = FakePin(2, None)
+bi0 = FakePin(32, None)
+bi1 = FakePin(33, None)
+bi2 = FakePin(25, None)
+bi3 = FakePin(26, None)
+bi4 = FakePin(27, None)
+bi5 = FakePin(14, None)
+bi6 = FakePin(12, None)
+bi7 = FakePin(13, None)
 
 class Empaquetado:
   def __init__(self,alarmas_depr, alarmas_vel, emp_por_hora, emp_por_robot):
@@ -185,12 +214,12 @@ def mostrar_atributo(empaquetado):
   print("\nEMPAQUETADOS POR HORA:", )
   print("Hora\t Cantidad")
   for i in range(len(empaquetado.get_empaquetados_por_hora())):
-    print(i,"\t",empaquetado.get_empaquetados_por_hora()[i])
+    print(i,"\t\t",empaquetado.get_empaquetados_por_hora()[i])
   
   print("\nEMPAQUETADOS POR ROBOT:", )
   print("Robot\t Cantidad")
   for i in range(len(empaquetado.get_empaquetados_por_robot())):
-    print(i,"\t",empaquetado.get_empaquetados_por_robot()[i])
+    print(i,"\t\t",empaquetado.get_empaquetados_por_robot()[i])
       
 
 empaquetado = Empaquetado(0,0,0,0)
